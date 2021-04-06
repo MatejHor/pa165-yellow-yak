f2b66f9dde421d2f1744592cd01f8ac1382a75cf
MatejHor <32205201+MatejHor@users.noreply.github.com>
2021-04-06 17:30:28 +0200

    Add CompetitionDaoTest, update GameDao

--- src/main/java/cz/fi/muni/pa165/yellow_yak/persistance/GameDao.java
+++ src/main/java/cz/fi/muni/pa165/yellow_yak/persistance/GameDao.java
@@ -9,15 +9,41 @@ import java.util.List;
  */
 public interface GameDao {
 
+    /**
+     * Create new game
+     * @param g game to create
+     */
     void create(Game g);
 
+    /**
+     * Return all game in db
+     * @return List of games
+     */
     List<Game> findAll();
 
+    /**
+     * Return game with specific id
+     * @param id specific id
+     * @return Game
+     */
     Game findById(Long id);
 
+    /**
+     * Remove game
+     * @param g game to remove
+     */
     void remove(Game g);
 
+    /**
+     * Update game
+     * @param g game to update
+     */
     void update(Game g);
 
+    /**
+     * Return all game with specific name in db
+     * @param name specific game name
+     * @return List of games
+     */
     Game findByName(String name) ;
 }
