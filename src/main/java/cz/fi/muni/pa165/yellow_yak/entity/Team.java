9e920e4a3dbfce50b4cbd7887d9d6acb1e4e1315
matej.knazik <matej.knazik@kiwi.com>
2021-04-06 09:19:25 +0200

    Team entity class + Team DAO layer interface

--- src/main/java/cz/fi/muni/pa165/yellow_yak/entity/Team.java
+++ src/main/java/cz/fi/muni/pa165/yellow_yak/entity/Team.java
@@ -1,5 +1,81 @@
 package cz.fi.muni.pa165.yellow_yak.entity;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+/**
+ * @author Matej Knazik
+ */
+@Entity
+@Table(name = "team")
 public class Team {
-    // TODO doplnit
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "team_id")
+    private Long id;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(referencedColumnName = "id", name = "game_id", nullable = false)
+    private Game game;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    @Column(name = "created_at", nullable = false)
+    private Date createdAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getCreated_at() {
+        return createdAt;
+    }
+
+    public void setCreated_at(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (!(obj instanceof Game))
+            return false;
+        Team other = (Team) obj;
+        return other.getId() == id;
+    }
+
 }
