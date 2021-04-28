212d7cd82e3a4df745821f1d2301723c1156e5d0
oreqizer <oreqizer@gmail.com>
2021-04-28 18:31:44 +0200

    team service tests

--- GamingManagement-service/src/test/java/cz/fi/muni/pa165/yellow_yak/service/TeamServiceTest.java
+++ GamingManagement-service/src/test/java/cz/fi/muni/pa165/yellow_yak/service/TeamServiceTest.java
@@ -1,7 +1,12 @@
 package cz.fi.muni.pa165.yellow_yak.service;
 
 import cz.fi.muni.pa165.yellow_yak.config.ServiceConfiguration;
+import cz.fi.muni.pa165.yellow_yak.entity.Competition;
+import cz.fi.muni.pa165.yellow_yak.entity.Game;
+import cz.fi.muni.pa165.yellow_yak.entity.Player;
 import cz.fi.muni.pa165.yellow_yak.entity.Team;
+import cz.fi.muni.pa165.yellow_yak.persistance.CompetitionDao;
+import cz.fi.muni.pa165.yellow_yak.persistance.PlayerDao;
 import cz.fi.muni.pa165.yellow_yak.persistance.TeamDao;
 import org.hibernate.service.spi.ServiceException;
 import org.mockito.InjectMocks;
@@ -18,8 +23,7 @@ import org.testng.annotations.Test;
 import java.util.Collections;
 import java.util.List;
 
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 /**
  * @author oreqizer
@@ -29,6 +33,10 @@ public class TeamServiceTest extends AbstractTestNGSpringContextTests {
 
     @Mock
     private TeamDao teamDao;
+    @Mock
+    private PlayerDao playerDao;
+    @Mock
+    private CompetitionDao competitionDao;
 
     @Autowired
     @InjectMocks
@@ -107,10 +115,131 @@ public class TeamServiceTest extends AbstractTestNGSpringContextTests {
         Assert.assertNull(teamTest);
     }
 
-    // TODO:
-    // - addPlayer
-    // - removePlayer
-    // - listByName
-    // - listByCompetition
-    // - listByPlayer
+    @Test
+    public void addPlayer() {
+        Player player = new Player();
+        player.setId(1L);
+        player.setUsername("xxx_BILLY_xxx");
+        player.setEmail("billy@killer.com");
+
+        // TODO check it was called...?
+    }
+
+    @Test(expectedExceptions = NullPointerException.class)
+    public void addPlayerNullTeam() {
+        Player player = new Player();
+        player.setId(1L);
+        player.setUsername("xxx_BILLY_xxx");
+        player.setEmail("billy@killer.com");
+
+        when(playerDao.findById(player.getId())).thenReturn(player);
+        when(teamDao.getById(team.getId())).thenReturn(team);
+        doThrow(NullPointerException.class).when(teamDao).addPlayer(null, player);
+
+        teamService.addPlayer(null, player.getId());
+    }
+
+    @Test(expectedExceptions = NullPointerException.class)
+    public void addPlayerNullPlayer() {
+        doThrow(NullPointerException.class).when(teamDao).addPlayer(team, null);
+
+        teamService.addPlayer(team.getId(), null);
+    }
+
+    @Test
+    public void removePlayer() {
+        Player player = new Player();
+        player.setId(1L);
+        player.setUsername("xxx_BILLY_xxx");
+        player.setEmail("billy@killer.com");
+
+        // TODO check it was called...?
+    }
+
+    @Test(expectedExceptions = NullPointerException.class)
+    public void removePlayerNullTeam() {
+        Player player = new Player();
+        player.setId(1L);
+        player.setUsername("xxx_BILLY_xxx");
+        player.setEmail("billy@killer.com");
+
+        when(playerDao.findById(player.getId())).thenReturn(player);
+        when(teamDao.getById(team.getId())).thenReturn(team);
+        doThrow(NullPointerException.class).when(teamDao).removePlayer(null, player);
+
+        teamService.removePlayer(null, player.getId());
+    }
+
+    @Test(expectedExceptions = NullPointerException.class)
+    public void removePlayerNullPlayer() {
+        doThrow(NullPointerException.class).when(teamDao).removePlayer(team, null);
+
+        teamService.removePlayer(team.getId(), null);
+    }
+
+    @Test
+    public void listByName() {
+        when(teamDao.getByName(team.getName())).thenReturn(Collections.singletonList(team));
+
+        List<Team> list = teamService.listByName(team.getName());
+
+        Assert.assertNotNull(list);
+        Assert.assertEquals(list.size(), 1);
+        Assert.assertEquals(list.get(0), team);
+    }
+
+    @Test(expectedExceptions = NullPointerException.class)
+    public void listByNameNull() {
+        doThrow(NullPointerException.class).when(teamDao).getByName(null);
+
+        teamService.listByName(null);
+    }
+
+    @Test
+    public void listByCompetition() {
+        Competition competition = new Competition();
+        competition.setId(1L);
+        competition.setName("YOLOFEST");
+
+        when(competitionDao.findById(competition.getId())).thenReturn(competition);
+        when(teamDao.getByCompetition(competition)).thenReturn(Collections.singletonList(team));
+
+        List<Team> list = teamService.listByCompetition(competition.getId());
+
+        Assert.assertNotNull(list);
+        Assert.assertEquals(list.size(), 1);
+        Assert.assertEquals(list.get(0), team);
+    }
+
+    @Test(expectedExceptions = NullPointerException.class)
+    public void listByCompetitionNull() {
+        doThrow(NullPointerException.class).when(teamDao).getByCompetition(null);
+
+        teamService.listByCompetition(null);
+    }
+
+    @Test
+    public void listByPlayer() {
+        Player player = new Player();
+        player.setId(1L);
+        player.setUsername("xxx_BILLY_xxx");
+        player.setEmail("billy@killer.com");
+
+        when(playerDao.findById(player.getId())).thenReturn(player);
+        when(teamDao.getByPlayer(player)).thenReturn(Collections.singletonList(team));
+
+        List<Team> list = teamService.listByPlayer(player.getId());
+
+        Assert.assertNotNull(list);
+        Assert.assertEquals(list.size(), 1);
+        Assert.assertEquals(list.get(0), team);
+    }
+
+    @Test(expectedExceptions = NullPointerException.class)
+    public void listByPlayerNull() {
+        doThrow(NullPointerException.class).when(teamDao).getByPlayer(null);
+
+        teamService.listByPlayer(null);
+    }
+
 }
