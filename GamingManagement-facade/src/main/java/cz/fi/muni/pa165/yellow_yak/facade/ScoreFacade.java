dd2badd9ff9febed2f7200e8ad629ac0a3e7c4ad
oreqizer <oreqizer@gmail.com>
2021-04-29 08:42:31 +0200

    comment out score facade

--- GamingManagement-facade/src/main/java/cz/fi/muni/pa165/yellow_yak/facade/ScoreFacade.java
+++ GamingManagement-facade/src/main/java/cz/fi/muni/pa165/yellow_yak/facade/ScoreFacade.java
@@ -15,42 +15,42 @@ public interface ScoreFacade {
     public List<GameDTO> getAllGame();
     public List<ScoreDTO> getPlayerScore(Long playerId, Long gameId, LocalDateTime oldest);
 
-    // TODO these need implementing / reviewing
-
-    /**
-     * Creates a new score
-     * @param competitionId competition ID
-     * @param playerId player ID
-     * @return the created score
-     */
-    public ScoreDTO create(Long competitionId, Long playerId);
-
-    /**
-     * Removes the score
-     * @param id id to remove
-     */
-    public void remove(Long id);
-
-    /**
-     * Finds a score by id
-     * @param id the ID to find
-     * @return the score
-     */
-    public ScoreDTO findById(Long id);
-
-    /**
-     * Lists score statistics for the player
-     * @param playerId player id to search by
-     * @param gameId game id to search by
-     * @return list of scores
-     */
-    public List<ScoreDTO> listByPlayerGame(Long playerId, Long gameId);
-
-    /**
-     * Lists score statistics for the competition
-     * @param competitionId id to search by
-     * @return list of scores
-     */
-    public List<ScoreDTO> listByCompetition(Long competitionId);
+//    // TODO these need implementing / reviewing
+//
+//    /**
+//     * Creates a new score
+//     * @param competitionId competition ID
+//     * @param playerId player ID
+//     * @return the created score
+//     */
+//    public ScoreDTO create(Long competitionId, Long playerId);
+//
+//    /**
+//     * Removes the score
+//     * @param id id to remove
+//     */
+//    public void remove(Long id);
+//
+//    /**
+//     * Finds a score by id
+//     * @param id the ID to find
+//     * @return the score
+//     */
+//    public ScoreDTO findById(Long id);
+//
+//    /**
+//     * Lists score statistics for the player
+//     * @param playerId player id to search by
+//     * @param gameId game id to search by
+//     * @return list of scores
+//     */
+//    public List<ScoreDTO> listByPlayerGame(Long playerId, Long gameId);
+//
+//    /**
+//     * Lists score statistics for the competition
+//     * @param competitionId id to search by
+//     * @return list of scores
+//     */
+//    public List<ScoreDTO> listByCompetition(Long competitionId);
 
 }
