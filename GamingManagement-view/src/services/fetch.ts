124fcbe5f430ceafd75dcb3f30dbb6f0094cf3b6
oreqizer <oreqizer@gmail.com>
2021-05-17 11:44:51 +0200

    feat(view): player service

--- GamingManagement-view/src/services/fetch.ts
+++ GamingManagement-view/src/services/fetch.ts
@@ -1,14 +1,49 @@
 import { load } from "./storage";
 
-export default async function fetch(input: RequestInfo, init: RequestInit = {}) {
+export enum Method {
+  GET = "GET",
+  POST = "POST",
+  DELETE = "DELETE",
+}
+
+export enum Status {
+  BAD_REQUEST = 400,
+  UNAUTHORIZD = 401,
+  NOT_FOUND = 404,
+  SERVER_ERROR = 500,
+}
+
+export default async function fetch<T>(path: string, init: RequestInit = {}): Promise<T> {
   const headers = init.headers ?? {};
   const token = load();
 
-  return window.fetch(input, {
+  return window.fetch(process.env.API_URL + path, {
     ...init,
     headers: {
       ...headers,
       Authorization: token !== null ? "" : `Bearer ${token}`,
     },
+  }).then(res => {
+    if (res.ok) {
+      return res.json();
+    }
+
+    if (res.status === Status.BAD_REQUEST) {
+      return Promise.reject(new Error("Invalid request parameters"));
+    }
+
+    if (res.status === Status.BAD_REQUEST) {
+      return Promise.reject(new Error("Unauthorized"));
+    }
+
+    if (res.status === Status.NOT_FOUND) {
+      return Promise.reject(new Error("Resource not found"));
+    }
+
+    if (res.status === Status.SERVER_ERROR) {
+      return Promise.reject(new Error("Unknown server error"));
+    }
+
+    return Promise.reject(new Error(`Unknown response code: ${String(res.status)}`))
   });
 }
--- GamingManagement-view/src/services/player.ts
+++ GamingManagement-view/src/services/player.ts
@@ -1,1 +1,42 @@
-export {};
+import fetch, { Method } from "./fetch";
+
+type Player = {
+  id: number;
+  username: string;
+  email: string;
+};
+
+// GET /players/:id
+export async function getPlayer(id: string): Promise<Player> {
+  return fetch(`/players/${id}`);
+}
+
+type CreatePlayerInput = {
+  username: string;
+  email: string;
+};
+
+// POST /players
+export async function createPlayer(input: CreatePlayerInput): Promise<Player> {
+  return fetch(`/players/`, {
+    method: Method.POST,
+    body: JSON.stringify(input),
+  });
+}
+
+// DELETE /players/:id
+export async function deletePlayer(id: string): Promise<null> {
+  return fetch(`/players/${id}`, {
+    method: Method.DELETE,
+  }).then(() => null);
+}
+
+// GET /players?username=
+export async function listPlayersByUsername(username: string): Promise<Player[]> {
+  return fetch(`/players?username=${username}`);
+}
+
+// GET /players?team=
+export async function listPlayersByTeam(team: string): Promise<Player[]> {
+  return fetch(`/players?team=${team}`);
+}
