2ee9bc14b785cdf8d53d251c0d89951924c1712a
oreqizer <oreqizer@gmail.com>
2021-05-20 20:39:29 +0200

    feat(view): fix authentication

--- GamingManagement-view/src/pages/Index.tsx
+++ GamingManagement-view/src/pages/Index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import * as Yup from "yup";
 import { Redirect, Route, Link, Switch } from "wouter";
 import { Alert, Button, Col, Container, Form, InputGroup, Nav, Navbar, Row } from "react-bootstrap";
-import {Formik, FormikHelpers} from "formik";
+import { Formik, FormikHelpers } from "formik";
 
 import Players from "./Players";
 import Teams from "./Teams";
@@ -22,6 +22,7 @@ type FormValues = {
 };
 
 const Index = () => {
+  const [isAdmin, setIsAdmin] = React.useState(false);
   const [state, setState] = useState();
   const success = isSuccess(state);
 
@@ -29,25 +30,24 @@ const Index = () => {
     () =>
       Yup.object().shape({
         email: Yup.string().email("Invalid email").required("Required"),
-        password: Yup.string().required("Required"),
+        password: Yup.string(),
       }),
     [],
   );
 
   const handleSubmit = React.useCallback((values: FormValues, form: FormikHelpers<FormValues>) => {
-    setState({ error: null }); // TODO enable this
-    // form.setSubmitting(true);
-    // login(values)
-    //   .then(({ token }) => {
-    //     save(token);
-    //     setState({ error: null });
-    //   })
-    //   .catch((error) => {
-    //     setState({ error });
-    //   })
-    //   .finally(() => {
-    //     form.setSubmitting(false);
-    //   });
+    form.setSubmitting(true);
+    login(values)
+      .then(({ token }) => {
+        save(token);
+        setState({ error: null });
+      })
+      .catch((error) => {
+        setState({ error });
+      })
+      .finally(() => {
+        form.setSubmitting(false);
+      });
   }, []);
 
   if (!success) {
@@ -85,23 +85,25 @@ const Index = () => {
                     </InputGroup>
                   </Form.Group>
 
-                  <Form.Group controlId="password">
-                    <Form.Label>Password</Form.Label>
-                    <InputGroup hasValidation>
-                      <Form.Control
-                        name="password"
-                        type="password"
-                        value={form.values.password}
-                        isInvalid={form.touched.password === true && form.errors.password != null}
-                        onChange={form.handleChange}
-                        onBlur={form.handleBlur}
-                        placeholder="Password"
-                      />
-                      <Form.Control.Feedback type="invalid">
-                        {form.errors.password}
-                      </Form.Control.Feedback>
-                    </InputGroup>
-                  </Form.Group>
+                  {isAdmin && (
+                    <Form.Group controlId="password">
+                      <Form.Label>Password</Form.Label>
+                      <InputGroup hasValidation>
+                        <Form.Control
+                          name="password"
+                          type="password"
+                          value={form.values.password}
+                          isInvalid={form.touched.password === true && form.errors.password != null}
+                          onChange={form.handleChange}
+                          onBlur={form.handleBlur}
+                          placeholder="Password"
+                        />
+                        <Form.Control.Feedback type="invalid">
+                          {form.errors.password}
+                        </Form.Control.Feedback>
+                      </InputGroup>
+                    </Form.Group>
+                  )}
 
                   {state?.error != null && <Alert variant="danger">{state.error.message}</Alert>}
 
@@ -111,6 +113,14 @@ const Index = () => {
                     disabled={!form.isValid || form.isSubmitting}
                   >
                     Sign in
+                  </Button>
+
+                  <Button
+                    className="mx-2"
+                    variant="outline-secondary"
+                    onClick={() => setIsAdmin((state) => !state)}
+                  >
+                    Login as {isAdmin ? "player" : "admin"}
                   </Button>
                 </Form>
               )}
