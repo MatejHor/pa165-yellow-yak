a685038258597e273ffb7cb98c772648aad439e2
oreqizer <oreqizer@gmail.com>
2021-05-23 13:37:01 +0200

    fix(view): fetch url fallback

--- GamingManagement-view/src/services/fetch.ts
+++ GamingManagement-view/src/services/fetch.ts
@@ -18,7 +18,7 @@ export default function fetch<T>(path: string, init: RequestInit = {}): Promise<
   const token = load();
 
   return window
-    .fetch(`${process.env.REACT_APP_API_URL ?? ""}${path}`, {
+    .fetch(`${process.env.REACT_APP_API_URL ?? "http://localhost/8080/pa165/rest"}${path}`, {
       ...init,
       headers: {
         ...headers,
