5adfd4be1325d1b894c0369c37746b546041a121
Lukáš Mikula <32229018+Lukinooo@users.noreply.github.com>
2021-05-23 20:42:40 +0200

    Add GameList, Add SearchGameForm

--- GamingManagement-view/src/pages/Games/components/SearchGameForm.tsx
+++ GamingManagement-view/src/pages/Games/components/SearchGameForm.tsx
@@ -1,5 +1,37 @@
+import React from "react";
+import { Alert, Form } from "react-bootstrap";
+
+import GameList from "./GameList";
+import {deleteGame, useGamesByName} from "../../../services/game";
+
 const SearchGameForm = () => {
-    return (<div>search</div>);
+    const ref = React.useRef<HTMLInputElement>(null);
+    const value = ref.current?.value ?? "";
+    const { data, error, revalidate } = useGamesByName(value);
+
+    const handleDelete = React.useCallback(
+        (id: number) => {
+            deleteGame(id)
+                .then(() => {
+                    return revalidate();
+                })
+                .catch(() => {});
+        },
+        [revalidate],
+    );
+    return (
+        <div className="my-3">
+            <h2 className="my-2">Search game</h2>
+            <Form.Group controlId="username">
+                <Form.Label>Name</Form.Label>
+                <Form.Control ref={ref} name="name" type="name" placeholder="e.g. Foxes vs Hawks" />
+            </Form.Group>
+
+            {error != null && <Alert variant="danger">{error.message}</Alert>}
+
+            {data && <GameList games={data} handleDelete={handleDelete} />}
+        </div>
+    );
 };
 
 export default SearchGameForm;
