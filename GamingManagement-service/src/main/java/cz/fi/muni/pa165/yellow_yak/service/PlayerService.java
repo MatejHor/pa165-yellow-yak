2dab729e217afbcfefb98af50c27df878d07edbd
Lukáš Mikula <32229018+Lukinooo@users.noreply.github.com>
2021-06-05 11:19:57 +0200

    Update PlayerService

--- GamingManagement-service/src/main/java/cz/fi/muni/pa165/yellow_yak/service/PlayerService.java
+++ GamingManagement-service/src/main/java/cz/fi/muni/pa165/yellow_yak/service/PlayerService.java
@@ -14,9 +14,10 @@ public interface PlayerService {
      * Creates a new player
      * @param name player name
      * @param email player email
+     * @param teamId ID of the team
      * @return the created player
      */
-    public Player create(@NotNull String name, @NotNull String email);
+    public Player create(@NotNull String name, @NotNull String email, Long teamId);
 
     /**
      * Removes the player
